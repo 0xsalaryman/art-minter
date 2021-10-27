48960346f44a2074357da1d43faaa065d59fb83a
Cain <cainbinadam@gmail.com>
2021-10-27 23:01:17 +0800

    Completed front-end

--- src/App.js
+++ src/App.js
@@ -1,68 +1,95 @@
 import 'bulma/css/bulma.min.css';
+import './App.css';
+
 import React from 'react';
 import p5 from 'p5';
-import './App.css';
-// import { sketch } from './algorithms/shapes.js';
-import meta from './img/metamask.png';
+
 import { ethers } from 'ethers';
+import { getAlgo, algoList } from "./utils/dictionary.js";
+import { connectWallet, getCurrentWalletConnected, mintNFT, waitTx } from "./utils/interact.js";
+
+import meta from './img/metamask.png';
+
+
 var $ = require( "jquery" );
-
-
 
 const App = () => {
 
-  // const [algorithm, setAlgorithm] = React.useState("Chromie Squiggle");
-  const [imgScript, setImgScript] = React.useState(initImg())
+  // States for art generation
+  
+  const [imgScript, setImgScript] = React.useState("")
   const [load, setLoad] = React.useState(1)
   const [canvas, setCanvas] = React.useState(0)
-  const [dropdown, setDropdown] = React.useState(0)
+  const [algo, setAlgo] = React.useState("Chromie Squiggle")
+  const [seed, setSeed] = React.useState("")
+  const algos = algoList()
+  const metamask = "Connect Metamask"
 
   React.useEffect(() => {
-    keyDown()
     toggleDropdown()
     globalToggleDropdown()
-    selectDropdown()
   }, []);
 
   React.useEffect(() => {
-    console.log(0)
     resizeEvent()
   }, [canvas]);
 
+  React.useEffect(() => {
+    keyDown()
+    selectDropdown()
+  }, [algo]);
 
-  
-  function generateSeed() {
-    let seed = Math.floor(Math.random() * 1e13);
-    let seedString = ethers.utils.hexlify(seed);
-    return ethers.utils.keccak256(seedString);
-  }
 
-  function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+  // States for wallet connection
+  const [walletAddress, setWallet] = React.useState("");
+  const [status, setStatus] = React.useState("");
+  const [button, setButton] = React.useState("");
 
-  function resizeEvent() {
-    window.addEventListener('resize', function(event) {
-      event.stopPropagation();
-      // console.log(1)
-      // console.log(canvas)
-      if (canvas == 1) {
-        let container = document.getElementById('shapes');
-        let windowHeight = container.clientHeight
-        console.log(windowHeight)
-        var iframe = document.getElementById("artframe");
-        var element = iframe.contentWindow.document.getElementById('defaultCanvas0').setAttribute("style",`height:${windowHeight}px`);
-        let width = iframe.contentWindow.document.getElementById('defaultCanvas0').clientWidth
-        // document.getElementById("shapes").setAttribute("style",`min-width:0`);
-        document.getElementById("shapes").style.width = `${width}px`
-        // document.getElementById("generate").style.minWidth = "0";
-        document.getElementById("generate").style.width = `${width}px`;
-        
-      }
+  React.useEffect(async () => { 
+    const {address, status} = await getCurrentWalletConnected();
+    setWallet(address)
+    // setStatus("Mint");
+    // setLoad("button") 
+    // if (address == "") {
+    //   setButton(true)
+    // } else {
+    //   setButton(false)
+    // }
+    // console.log(button)
     
-    }, true);
+    addWalletListener();
+  }, []);
+  
+  const connectWalletPressed = async () => { 
+    const walletResponse = await connectWallet();
+
+    setWallet(walletResponse.address);
+    // if (walletResponse.address == "") {
+    //   setButton(true)
+    // } else {
+    //   setButton(false)
+    // }
+    
+  };
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          // setStatus("");
+          // setButton(true)
+        } else {
+          setWallet("");
+          // setStatus("");
+          // setButton(false)
+        }
+      });
+    }
   }
 
+
+  // Generate new art when space or enter is pressed
   function keyDown() {
     document.addEventListener("keydown", function(e) {
       if ((e.code == "Space" && e.target == document.body) || e.code == "Enter") {
@@ -73,67 +100,16 @@ const App = () => {
     });
   }
 
-  function selectDropdown() {
-    var items = document.getElementsByClassName('dropdown-item');
-    console.log(items)
-  }
-
-  function initImg() {
-    let img = (
-      <div id="image-container">
-        <img src="initial.gif" id="init"/>
-      </div>
-    )
-    return img
-  }
-
-  function save() {
-    var iframe = document.getElementById("artframe");
-    var element = iframe.contentWindow.document.getElementById('defaultCanvas0');
-    // console.log(element)
-    var dataURL = element.toDataURL()
-    // // console.log(dataURL)
-    // saveAjax(dataURL)
-  }
-
-  function waitForCanvas(){
-    var iframe = document.getElementById("artframe");
-    var checkExist = setInterval(function() {
-        if (iframe.contentWindow.document.querySelectorAll('#defaultCanvas0').length) {
-          clearInterval(checkExist);
-          setLoad(-1)
-          setCanvas(1)
-          let width = iframe.contentWindow.document.getElementById('defaultCanvas0').clientWidth
-          document.getElementById("shapes").setAttribute("style",`min-width:0`);
-          document.getElementById("shapes").style.width = `${width}px`;
-          document.getElementById("generate").setAttribute("style",`min-width:0`)
-          document.getElementById("generate").style.width = `${width}px`;
-        }
-    }, 100);
-    
-  }
-
-  function saveAjax(dataUrl) {
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:8000/script.php",
-      data: { 
-          imgBase64: dataUrl
-      }
-    }).done(function(o) {
-      console.log('saved'); 
-    });
-  }
-
+  //Toggle dropdown when clicked
   function toggleDropdown(){
     let dropdown = document.getElementById("algo")
     dropdown.addEventListener("click", function(ev) {
       ev.stopPropagation();
       dropdown.classList.toggle("is-active");
-      console.log("hello")
     });
   }
 
+  // Toggle Dropdown when other areas are clicked
   function globalToggleDropdown(){
     let dropdown = document.getElementById("algo")
     document.addEventListener("click", function(ev) {
@@ -142,32 +118,173 @@ const App = () => {
     })
   }
 
+  // Set algo state when selected from Dropdown
+  function selectDropdown() {
+    var items = document.getElementsByClassName('dropdown-item');
+    let dropdown = document.getElementById("algo")
+    items.forEach(function (el) {
+      el.onclick = function (event) {
+        event.stopPropagation();
+        dropdown.classList.remove('is-active')
+        let active = document.getElementsByClassName("dropdown-item is-active")
+        if (active.length > 0) {
+          active[0].classList.remove("is-active")
+        }
+        el.classList.toggle("is-active")
+        var text = el.innerText || el.textContent;
+        setAlgo(text)
+      }
+    });
+  }
 
+  // Initial loading image
+  // function initImg() {
+    // let img = (
+    //   <div id="image-container">
+    //     <img src="initial2.gif" id="init"/>
+    //   </div>
+    // )
+    // return img
+  //   return ""
+  // }
+
+  // Function to save rendered canvas into image
+  function save() {
+    var iframe = document.getElementById("artframe");
+    // let canvas = iframe.contentWindow.document.getElementById('defaultCanvas0')
+    let canvases = iframe.contentWindow.document.getElementsByTagName('canvas')
+    let canvas = canvases[0]
+    // var element = iframe.contentWindow.document.getElementById('defaultCanvas0');
+    // console.log(element)
+    var dataURL = canvas.toDataURL()
+    // // console.log(dataURL)
+    saveAjax(dataURL, seed, "../tmp/")
+  }
+
+  function saveAjax(_data_url, _name, _location) {
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:8000/php/script.php",
+      data: { 
+          data_url: _data_url,
+          name: _name,
+          location: _location
+      }
+    }).done(function(o) {
+      console.log('saved'); 
+    });
+  }
+
+  // Helper function to resize artboard
+  function resize() {
+    var iframe = document.getElementById("artframe");
+    // let canvas = iframe.contentWindow.document.getElementById('defaultCanvas0')
+    let canvases = iframe.contentWindow.document.getElementsByTagName('canvas')
+    if (canvases.length > 0) {
+    } else {
+      canvases = iframe.contentWindow.document.getElementsByTagName('svg')
+    }
+    let canvas = canvases[0]
+    // console.log(canvases)
+    
+    let board = document.getElementById("board")
+    let button = document.getElementById("generate")
+    let container = document.getElementById("container")
+
+    let margin = 12
+    let ww = container.clientWidth - margin*2
+    let wh = window.innerHeight - 250
+    let bw = board.clientWidth
+    let bh = board.clientHeight
+    let cw = canvas.clientWidth
+    let ch = canvas.clientHeight
+
+    let predicted_cw = cw/ch * bw.toFixed(2)
+    // console.log(predicted_cw, ww, wh)
+
+
+    if (predicted_cw - ww > 1) {
+      // console.log("true")
+      canvas.setAttribute("style",`width: ${ww}px`);
+      board.setAttribute("style",`height: ${canvas.clientHeight}px`);
+      board.style.width = `${canvas.clientWidth}px`;
+      button.setAttribute("style",`width: ${canvas.clientWidth}px`);
+    } else {    
+      canvas.setAttribute("style",`height: ${wh}px`);
+      button.setAttribute("style",`width: ${canvas.clientWidth}px`);
+      // console.log(canvas.clientWidth)
+      board.setAttribute("style",`width: ${canvas.clientWidth}px`);
+      board.style.height = `${canvas.clientHeight}px`;
+      
+    }
+  }
+
+  // Helper function to wait for iframe to render
+  function waitForCanvas(){
+    var iframe = document.getElementById("artframe");
+    var checkExist = setInterval(function() {
+      
+        // if (iframe.contentWindow.document.querySelectorAll('#defaultCanvas0').length) {
+        if (iframe.contentWindow.document.querySelectorAll('canvas').length) {
+          clearInterval(checkExist);
+          setLoad(-1)
+          setCanvas(1)
+          resize()
+          console.log(true)
+        } 
+        else if (iframe.contentWindow.document.querySelectorAll('svg').length) {
+          clearInterval(checkExist);
+          setLoad(-1)
+          setCanvas(1)
+          resize()
+        } 
+    }, 100);
+    document.getElementById("mint").removeAttribute("disabled")
+  }
+
+  // Resize art board when window is resized
+  function resizeEvent() {
+    window.addEventListener('resize', function(event) {
+      event.stopPropagation();
+      if (canvas == 1) {
+        resize()
+      }
+    }, true);
+  }
+
+  // Generate seed to generate art
+  function generateSeed() {
+    let seed = Math.floor(Math.random() * 1e13);
+    let seedString = ethers.utils.hexlify(seed);
+    return ethers.utils.keccak256(seedString);
+  }
+
+  // Compose the HTML for p5 execution
   function composeImgScript(seed, filename) {
     return (
       `<html>
         <head>
-            <script src='https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5.js'>
-            </script>
-            <script>
-              let tokenData = {
-                hash: "${seed}",
-                hashes: ["${seed}"],
-              };
-            </script>
-            <script src="algorithms/${filename}"></script>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <script src="https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5.js"></script>
         </head>
-        <body style="margin: 0">
-            <main>
-            </main>
-        </body>
+        <body style="margin: 0"></body>
+        <script>
+          let tokenData = {
+            hash: "${seed}",
+            hashes: ["${seed}"],
+          };
+        </script>
+        <script src="algorithms/${filename}"></script>
       </html>`
     )
   }
 
+  // Create the iframe to run p5 script
   function composeIframe(imgScript) {
-    let container = document.getElementById('shapes');
-    let windowHeight = container.clientHeight
+    let container = document.getElementById('board');
+    // let windowHeight = container.clientHeight
     // console.log(windowHeight)
     return (
       <iframe
@@ -187,27 +304,21 @@ const App = () => {
     )
   }
 
-  // function getAlgo() {
-  //   var e = document.getElementById("algo");
-  //   console.log(e.selectedIndex)
-  //   var strUser = e.options[e.selectedIndex].text;
-  //   console.log(strUser)
-  //   console.log(e)
-  // }
-
+  // Generate new art
   function generate() {
     setLoad(1)
-    // let algorithm = getAlgo()
-
+    let algorithm = getAlgo(algo)
 
     let seed = generateSeed()
-    let imgScript = composeImgScript(seed, "fidenza.js")
+    setSeed(seed)
+
+    let imgScript = composeImgScript(seed, algorithm)
     let iFrame = composeIframe(imgScript)
     setImgScript(iFrame)
     // waitForCanvas()
   }
 
-
+  //Return app layout
   return (
     <div id="top" style={{backgroundColor: "#282c34"}}>
         <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
@@ -218,8 +329,15 @@ const App = () => {
 
             <div class="navbar-item">
               <div class="buttons">
-                <a class="button is-light is-dark">
-                  {"Connect Metamask \u00A0"} <img id="meta" src={meta}/>
+                <a class="button is-light is-dark" onClick={connectWalletPressed}>
+                {walletAddress.length > 0 ? (
+                  "Connected: " +
+                  String(walletAddress).substring(0, 6) +
+                  "..." +
+                  String(walletAddress).substring(38)
+                ) : ("Connect Metamask \u00A0"
+                )}
+                {walletAddress.length > 0 ? ("") : (<img id='meta' src={meta}/>)}
                 </a>
               </div>
             </div>
@@ -227,7 +345,7 @@ const App = () => {
             <div class="dropdown navbar-item" id="algo">
               <div class="dropdown-trigger">
                 <button class="button is-dark" aria-haspopup="true" aria-controls="dropdown-menu">
-                  <span>Dropdown button</span>
+                  <span>{algo}</span>
                   <span class="icon is-small">
                     <i class="fas fa-angle-down" aria-hidden="true"></i>
                   </span>
@@ -235,54 +353,44 @@ const App = () => {
               </div>
               <div class="dropdown-menu" id="dropdown-menu" role="menu">
                 <div class="dropdown-content">
-                  <a href="#" class="dropdown-item">
-                    Dropdown item
-                  </a>
-                  <a class="dropdown-item">
-                    Other dropdown item
-                  </a>
-                  <a href="#" class="dropdown-item is-active">
-                    Active dropdown item
-                  </a>
-                  <a href="#" class="dropdown-item">
-                    Other dropdown item
-                  </a>
-                  <hr class="dropdown-divider"/>
-                  <a href="#" class="dropdown-item">
-                    With a divider
-                  </a>
+                  {algos.map(function (x, i) { 
+                    return (
+                      <a class="dropdown-item">
+                        {x}
+                      </a>
+                    )
+                  })}
+      
                 </div>
               </div>
             </div>
           </div>
         
         </nav>
-        <div class="container">
-          <div class="columns is-vcentered">
-            <div class="column">
-              <div class="content">
-                <h1>Mint "Art".</h1>
-                <br/>
-                <p>How it works:</p>
-                <ol>
-                  <li>Connect Metamask</li>
-                  <li>Select the algorithm</li>
-                  <li>Click on the canvas on the right to generate the art</li>
-                  <li>Mint when you see something you like</li>
-                </ol>
-              </div>
-              <a class="button is-info" onClick={save}>
-                Mint
-              </a>
-              
+        <div class="columns is-vcentered">
+          <div class="column">
+            <div class="content">
+              <h1>Mint "Art".</h1>
+              <br/>
+              <p>How it works:</p>
+              <ol>
+                <li>Select algorithm</li>
+                <li>Generate the art</li>
+                <li>Mint if you find something you like</li>
+              </ol>
             </div>
-            <div class="column">
-              <div id="shapes" class="art">
-                <a class="button is-loading" id="clickable" style={{zIndex:load}}>
-                </a>
-                {imgScript}
-              </div>
-              
+            <a  id="mint" class="button is-info" disabled={true} onClick={save}>
+              Upload
+            </a>
+          </div>
+          <div class="column is-two-thirds" id="container">
+            <div id="board" class="art">
+              {/* <a class="button is-loading" id="loader" disabled style={{zIndex:load}}>
+              </a> */}
+              <img src="initial2.gif" id="init" style={{zIndex:load}}/>
+              {imgScript}
+            </div>
+            <div>
               <a class="button is-small is-dark" id="generate" onClick={generate}>
                 Generate (⎵ or ↵)
               </a>
--- src/php/script.php
+++ src/php/script.php
@@ -8,14 +8,17 @@
 	// $file = UPLOAD_DIR . uniqid() . '.png';
 	// $success = file_put_contents($file, $data);
 	// print $success ? $file : 'Unable to save the file.';
-    $img = $_POST['imgBase64'];
+    $img = $_POST['data_url'];
     $img = str_replace('data:image/png;base64,', '', $img);
     $img = str_replace(' ', '+', $img);
     $fileData = base64_decode($img);
     print $img;
     echo $img;
     //saving
-    $fileName = 'photo.png';
+    $location = $_POST['location'];
+    $name = $_POST['name'];
+
+    $fileName = $location . $name;
     file_put_contents($fileName, $fileData);
     // ob_start();
     // var_dump($img);
